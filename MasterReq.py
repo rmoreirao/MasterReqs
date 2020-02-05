f5a8931459a44b45c127f59c51af97e646aaf075
Rodrigo Moreirao <rmoreirao@gmail.com>
2020-02-05 09:07:30 +0100

    Adding first version of histogram

--- MasterReq.py
+++ MasterReq.py
@@ -1,3 +1,7 @@
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+
 class NodeData(object):
     def __init__(self, key,count):
         self.key = key
@@ -31,17 +35,13 @@ class LinkedList(object):
         self.head = None
         self.size = 1
 
-    def prepend(self, node_data):
-        self.head = Node(node_data, self.head)
-        self.size += 1
-
     def append(self,node_data):
         if not self.head:
             self.head = Node(node_data)
             return
         curr = self.head
-        while curr.next:
-            curr = curr.next
+        while curr.get_next():
+            curr = curr.get_next()
         curr.set_next(Node(node_data))
 
     def get_size(self):
@@ -53,7 +53,7 @@ class LinkedList(object):
     def find_node_data(self,node_data_key):
         curr = self.head
         while curr and curr.get_node_data().get_key() != node_data_key:
-            curr = curr.next
+            curr = curr.get_next()
         if curr:
             return curr.get_node_data()
         return None
@@ -63,48 +63,12 @@ class LinkedList(object):
         str = ''
         while curr:
             str = "{} [{}, {}]".format(str,curr.get_node_data().get_key(),curr.get_node_data().get_count())
-            curr = curr.next
+            curr = curr.get_next()
             if curr:
                 str = '{} ->'.format(str)
         return str
 
-# articles_sample_data = {1:'asdnjsadlkjfalkjdsflkjahsdlhflasdhfasdf',
-#             2:'slkdjflskjdflsjdflsdf',
-#             3:'sdçlfsljdflksjdfkjdlsjldkfjslkdjf'}
-#
-# def read_articles_sample_data():
-#     articles_hash = {}
-#     for key, phrase in articles_sample_data.items():
-#         for letter in phrase:
-#             letter_list = articles_hash.get(key,[])
-#             letter_list.append(letter)
-#             articles_hash[key] = letter_list
-#
-#     return articles_hash
-#
-# articles_words_hash = {}
-#
-# for key,words in read_articles_sample_data().items():
-#     for word in words:
-#         letter_list = articles_words_hash.get(word, LinkedList())
-#         node_data = letter_list.find_node_data(key)
-#         if node_data:
-#             node_data.increment_count()
-#         else:
-#             letter_list.append(NodeData(key, 1))
-#
-#         articles_words_hash[word] = letter_list
-#
-# for key,value in articles_words_hash.items():
-#     print('[{}] ->{}'.format(key,value))
-
-import re
-
-
-
-def remove_xml_tags(text):
-    return re.sub('<[^<]+>', "")
-
+# a hash with key = document number and value = linked list of all the word appearances
 articles_words_hash = {}
 current_doc = 0
 
@@ -112,13 +76,14 @@ with open("txt-for-assignment-data-science.txt") as f:
     for line in f.readlines():
         #clean the line
         line = line.replace('&amp;','').strip()
-        # line.startswith('<') and line.endswith('>')
+        # check if it is a Xml tag
         if re.match('<[^<]+>',line):
             if line.startswith('<doc>'):
                 current_doc += 1
         else:
             words = line.lower().strip().split(' ')
             for word in words:
+                #cleaning the words
                 word = re.sub('[^A-Za-z0-9]+', '', word)
                 if word:
                     words_list = articles_words_hash.get(word, LinkedList())
@@ -130,5 +95,28 @@ with open("txt-for-assignment-data-science.txt") as f:
 
                     articles_words_hash[word] = words_list
 
-for key,value in articles_words_hash.items():
-    print('[{}] ->{}'.format(key,value))
+words_count_distribution = []
+
+#Print the words and the associated documents
+for doc_number,linked_list in articles_words_hash.items():
+    print('[{}] ->{}'.format(doc_number,linked_list))
+
+    curr = linked_list.get_head()
+    while curr:
+        words_count_distribution.append(curr.get_node_data().get_count())
+        curr = curr.get_next()
+
+bins = np.arange(max(words_count_distribution)) - 0.5
+print (bins)
+# plt.hist(words_count_distribution, bins, alpha=0.75, color='g', linewidth=0.75, edgecolor='black')
+# plt.title("(b)")
+# plt.xlabel("Total count of words")
+# plt.ylabel("Frequency of total count")
+# x1,x2,y1,y2 = plt.axis()
+# plt.axis((1,21,y1,y2))
+# plt.xticks(np.arange(1, 20, 2))
+# plt.xlim([0.5, 20.5])
+# frequency, bins = np.histogram(words_count_distribution, bins=20, range=[1, 25])
+plt.hist(words_count_distribution, bins=bins, alpha=0.5, edgecolor='gray',  linewidth=1)
+plt.xticks(range(1,25,2))
+plt.show()
